4675b16a51cc80764ab8a357379b3f7251ff8809
RAIMUNDO HEITOR SIQUEIRA DE MIRANDA 114110485 <raimundohsm@lcc2-35.lcc.ufcg.edu.br>
2018-07-18 14:04:53 -0300

    prolog file

---
+++ prolog/terminaltcg.pl
@@ -0,0 +1,8 @@
+main :-
+    nl, print("Novo Jogo!"), nl,
+    print("Escolha uma opcao (digite o numero): 1. Jogar | 2. Ajuda"),nl,
+    % read_line_to_codes(user_input, T1),
+    % string_to_atom(T1, A),
+    % menu_opcao(A),
+    main,
+    halt(0).
